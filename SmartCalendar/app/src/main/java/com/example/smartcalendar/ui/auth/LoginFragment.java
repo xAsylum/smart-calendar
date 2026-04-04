04757194e811fd529b70c949dca8848702ead925
xAsylum <jakub.wieliczko@student.uj.edu.pl>
2026-04-05 00:05:35 +0200

    Android app init

---
+++ SmartCalendar/app/src/main/java/com/example/smartcalendar/ui/auth/LoginFragment.java
@@ -0,0 +1,45 @@
+package com.example.smartcalendar.ui.auth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.smartcalendar.R;
+import com.example.smartcalendar.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        binding.buttonLogin.setOnClickListener(v -> {
+            NavHostFragment.findNavController(LoginFragment.this)
+                    .navigate(R.id.action_FirstFragment_to_SecondFragment);
+        });
+
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
